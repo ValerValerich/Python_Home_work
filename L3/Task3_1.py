035827dcf2e63c549ce7f985f71742a61820ac1f
ValerValerich <zvalera1991@mail.ru>
2022-12-27 00:29:25 +0300

    Task3_1 OK

---
+++ L3/Task3_1.py
@@ -0,0 +1,26 @@
+# 1. Задайте список, состоящий из произвольных чисел, количество задаёт пользователь.
+# Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётных позициях(не индексах)
+
+import os, random
+def clear(): return os.system('cls')
+
+
+clear()
+
+def create_list(x): # создание списка
+    random_lst=random.sample(range(-100, 101), k=x) # предопределенный диапазон чисел
+    return random_lst
+
+def sum_odd_position (lst): # суммирование из четных индексов (нечетных позиций)
+    x=0
+    for i in range(0, len(lst), 2):
+        x+=lst[i]
+    return x
+
+
+lenght_list=int(input("Введите длину списка\n"))
+lst=create_list(lenght_list)
+special_sum=sum_odd_position(lst)
+
+print(lst)
+print(special_sum)
